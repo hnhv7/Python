65593801862e1c23e768bbc77bfb3c9f02663cfa
Harry Nantz <Harry.Nantz@Lnttechservices.com>
2018-01-25 10:16:55 -0600

    split one function into 3; added comments

--- Cryptopals/crypto1.py
+++ Cryptopals/crypto1.py
@@ -1,23 +1,25 @@
 import logging
 import sys
-#ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-
-logging.basicConfig(level=logging.CRITICAL, format='%(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 logging.debug('Start of program')
 
+
+#############################################
+#
+#  Takes in a hex number and returns
+#        a binary number
+#
+#############################################
                     
 def hex2bin(number):
-  base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"
   binnum = ""
-  count = 0
-  newWord = ""
-  final = ""
   binholder = ""
   
-  for i in number:
-    if i == "a" or i == "A":
-      binnum = binnum + "1010"
-    elif i == "b" or i == "B":
-      binnum = binnum + "1011"
+  for i in number:                     # Loop takes first number in hex number
+    if i == "a" or i == "A":           # and checks whether it's A-F
+      binnum = binnum + "1010"         # If it isn't, uses bin() function
+    elif i == "b" or i == "B":         # and trims off "0b" then adds padding 0s
+      binnum = binnum + "1011"         # if necessary
     elif i == "c" or i == "C":
       binnum = binnum + "1100"
     elif i == "d" or i == "D":
@@ -30,20 +32,42 @@ def hex2bin(number):
       binholder = bin(int(i))[2:]
       binholder = binholder.rjust(4,"0")
       binnum = binnum + binholder
-    logging.debug('binholder:' + str(binholder))
-    
-  while count < len(binnum):
-   newWord = binnum[count:count+6:1]
-   logging.debug('binnum:' + str(binnum))
-   newWord = int(newWord, 2)
-   logging.debug('newWord:' + str(newWord))
-   holder = base64[newWord]
-   logging.debug('holder :' + str(holder))
-   final = final + holder
-   count = count +6
+  return binnum
 
+#############################################
+#
+#  Takes in a binary number and returns
+#        a Base-64 number
+#
+#############################################
+
+def bin2base64(binnum):
+  base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"
+  count = 0
+  final = ""
+
+  while count < len(binnum):              # Grabs 6 bits at a time
+    temp = binnum[count:count + 6:1]      # and converts them into Base-64
+    temp = int(temp, 2)                   # Then adds it to return value, "final"
+    holder = base64[temp]
+    final = final + holder
+    count = count + 6
   return final
-      
+
+
+#############################################
+#
+#  Takes in a hex number and returns
+#        a Base-64 number
+#
+#############################################
+
+def hex2base64(number):
+  binnum = hex2bin(number)
+  return_value = bin2base64(binnum)
+  return return_value
+  
+
 
 
 try:
@@ -59,6 +83,7 @@ while (loopnum != 0):
     if num not in "0123456789abcdefABCDEF":
       print("you chose an invalid number")
       sys.exit()
-  print ("That number in Base-64 is: " + hex2bin(number))
+  print ("That number in Binary is: " + hex2bin(number))
+  print ("That number in Base-64 is: " + hex2base64(number))
 
 
